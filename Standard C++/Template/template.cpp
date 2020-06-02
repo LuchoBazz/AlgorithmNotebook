b405561a201af0cb8e2d2b55d62272b9a037dd48
Luis Miguel Baez Aponte <es.luismiguelbaez@gmail.com>
2020-06-02 11:55:28 -0500

    Template was updated in c++

--- Standard C++/Template/template.cpp
+++ Standard C++/Template/template.cpp
@@ -1,5 +1,5 @@
 // Made by Luis Miguel Baez
-// lmbaeza@unal.edu.co
+// es.luismiguelbaez@gmail.com
 
 #include <bits/stdc++.h>
  
@@ -7,19 +7,42 @@ using namespace std;
 
 #define debug(...) "[" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
 
+string to_string(string s) {
+  return '"' + s + '"';
+}
+
+string to_string(bool b) {
+  return (b ? "true" : "false");
+}
+ 
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+  return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+ 
+template <typename A, typename T>
+string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for (const T &x : v) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string((T)x);
+  }
+  res += "}";
+  return res;
+}
+
 template<class T>
 void println(const vector<T> &data) {
-    int n = data.size();
-    
-    cout << "values: [";
-    for(int i = 0; i < data.size(); ++i) {
-        if(i != (n-1)) {
-            cout << data[i] << ", ";
-        } else {
-            cout << data[i];
-        }
-    }
-    cout << "]" <<endl;
+    cout << to_string<vector<T>, T>(data) <<endl;
+}
+
+template<class T>
+void println(const T &data) {
+    cout << to_string((T)data) << endl;
 }
  
 #define endl '\n'
@@ -40,10 +63,6 @@ void println(const vector<T> &data) {
 #define S  second
 #define PB push_back
 #define MP make_pair
-
-#define PI   (2*acos(0.0))
-#define gcd  __gcd
-#define eps  1e-9
 
 const int MOD = 1e9+7;
 const int oo  = int(1e9 + 20);
