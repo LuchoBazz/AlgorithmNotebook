edfcf01c1230d553f6140d8febeecd83aa0258c1
Luis Miguel Baez Aponte <es.luismiguelbaez@gmail.com>
2020-07-25 15:53:06 -0500

    Some files name was updated, Segment tree and template were updated

--- Standard C++/Template/debugging.cpp
+++ Standard C++/Template/debugging.cpp
@@ -38,7 +38,54 @@ string to_string(const bitset<N> v) {
     return res;
 }
 
-// add to_string here
+template<typename K, typename V>
+//string to_string(const map<K, V> mp) {
+string to_string(const unordered_map<K, V> mp) {
+    bool first = true;
+    string res = "{";
+    for (const auto &[k, v] : mp) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string((K)k) + ": " + to_string((V)v);
+    }
+    res += "}";
+    return res;
+}
+
+template<typename K>
+//string to_string(const set<K> st) {
+string to_string(const unordered_set<K> st) {
+    bool first = true;
+    string res = "{";
+    for (const auto &k : st) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string((K)k);
+    }
+    res += "}";
+    return res;
+}
+
+template<typename T, size_t N>
+string to_string(const array<T, N> arr) {
+    bool first = true;
+    string res = "{";
+    for (const T &x :arr) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string((T)x);
+    }
+    res += "}";
+    return res;
+}
+
+// add template here
 
 template <typename T>
 string to_string(const vector<T> &v) {
